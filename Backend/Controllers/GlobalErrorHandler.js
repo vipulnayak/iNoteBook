3d32c281c7f45402b39d153b3a009a3cb3a5e6ab
Vipul <vipulnayak1192@gmail.com>
2023-08-14 22:23:41 +0530

    ready for development

--- Backend/Controllers/GlobalErrorHandler.js
+++
@@ -1,8 +0,0 @@
-module.exports=(error,req,res,next)=>{
-    error.statuscode = error.statuscode || 500;
-    error.status =error.status ||"error";
-    res.status(error.statuscode).json({
-      status:error.statuscode,
-      message:error.message
-    });
-}
