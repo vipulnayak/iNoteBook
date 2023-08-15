c269fe2bc0b4130428e40a9cd976fea2da9f9135
Vipul <vipulnayak1192@gmail.com>
2023-08-15 13:13:03 +0530

    lazy(code splitting)added

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,60 @@
-import './App.css';
-import { useState } from 'react';
-import {  Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import NoteState from './context/NoteState';
-import Alert from './components/Alert';
-import Login from './components/Login';
-import Signup from './components/SignUp';
+import "./App.css";
+import { lazy, Suspense } from "react";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import NoteState from "./context/NoteState";
+import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/SignUp";
+
+const Home = lazy(() => import("./components/Home"));
+const About = lazy(() => import("./components/About"));
+const Redirect = lazy(() => import("./Components/Redirect"));
 
 function App() {
-  const [alert, setAlert] = useState(null)
-  const showAlert=(message,type)=>{
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
-    })
+      msg: message,
+      type: type,
+    });
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 1500);
-  }
+  };
   return (
     <>
-      <NoteState>
-          <Navbar/>
-          <Alert alert={alert}/>
-          <div className="container">
-            <Routes>
-              <Route exact path="/" element={<Home showAlert={showAlert} />} />
-              <Route exact path="/about" element={<About />} />
-              <Route exact path="/login" element={<Login showAlert={showAlert}/>} />
-              <Route exact path="/signup" element={<Signup showAlert={showAlert}/>} />
-            </Routes>
-          </div>
-      </NoteState>
+      <div className="App">
+        <NoteState>
+          <Suspense fallback={<h1 className="text-center">Loading....</h1>}>
+            <Navbar />
+            <Alert alert={alert} />
+            <div className="container">
+              <Routes>
+                <Route
+                  exact
+                  path="/"
+                  element={<Home showAlert={showAlert} />}
+                />
+                <Route exact path="/about" element={<About />} />
+                <Route
+                  exact
+                  path="/login"
+                  element={<Login showAlert={showAlert} />}
+                />
+                <Route
+                  exact
+                  path="/signup"
+                  element={<Signup showAlert={showAlert} />}
+                />
+                <Route path="*" element={<Redirect />} />
+              </Routes>
+            </div>
+          </Suspense>
+        </NoteState>
+      </div>
     </>
-
   );
 }
 
